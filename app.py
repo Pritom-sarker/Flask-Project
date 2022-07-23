61cb1f1f8c88b1a491c5b9a01407fe05dce06768
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2022-07-23 19:51:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
- 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
+from gevent.pywsgi import WSGIServer
+
 app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
-# main driver function
+
+@app.route('/api', methods=['GET'])
+def index():
+    return "Hello, World!"
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run(host="149.28.29.54", port=8000, debug=True)
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('149.28.29.54', 5000), app)
+    http_server.serve_forever()
