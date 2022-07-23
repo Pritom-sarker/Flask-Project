3ac1da6a4ad318e9e09eb7594682d2b27743a4e8
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2022-07-23 19:59:25 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask
-from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
-def index():
-    return "Hello, World!"
+@app.route("/")
+def hello():
+	return "Hello Geeks!! from Google Colab"
 
-if __name__ == '__main__':
-    # Debug/Development
-    # app.run(debug=True, host="0.0.0.0", port="5000")
-    # Production
-    http_server = WSGIServer(('149.28.29.54', 2000), app)
-    http_server.serve_forever()
+if __name__ == "__main__":
+    app.run(host="149.28.29.54", port=400)
