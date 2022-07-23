8f1f07fe5c3123b4e17abc2fbe9601ca4e1a3531
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2022-07-23 20:10:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/buy")
 def hello():
-	return "Hello Geeks!! from Google Colab"
+	return "{}".format(['Buy XAUUSD 1708.6 1710.0 1703.0 0.01','Buy XAUUSD 0 1710.0 1703.0 0.01','sell XAUUSD 1708.6 1706.0 1713.0 0.1' ][random.randint(0,2)])
+
+
 
 if __name__ == "__main__":
-    app.run(host="149.28.29.54", port=400)
+    app.run(host="149.28.29.54", port=300)
